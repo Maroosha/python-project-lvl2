e8ccac4f8f97d76d548ae74dbc34f7bc64522fdf
Mariia Bilinska <mariiaklochko@gmail.com>
2021-12-01 14:15:19 +0000

    Step 4: add tests

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,8 @@
+from gendiff.generate_diff import generate_diff
+
+
+def test_generate_diff():
+    filepath1 = 'tests/fixtures/file1.json'
+    filepath2 = 'tests/fixtures/file2.json'
+    correct_answer = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+    assert generate_diff(filepath1, filepath2) == correct_answer
