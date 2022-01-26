4c6b14ab2176573096df3158afd8b4559998a892
Mariia Bilinska <mariiaklochko@gmail.com>
2022-01-26 12:40:16 +0000

    Edit gendiff.py module

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,12 +6,64 @@ Acceptable file formats: .JSON, .YAML, .YML
 
 # !/usr/bin/env python3
 
-from typing import Dict, OrderedDict
+from typing import Dict
 from gendiff.parser.data_parser import parse
 from gendiff.parser.file_reader import get_raw_data, get_format
 from gendiff.formatters.json_formatter import format_json
 from gendiff.formatters.stylish_formatter import format_stylish
 from gendiff.formatters.plain_formatter import format_plain
+
+
+def get_difference_pair(key, data1, data2):
+    """
+    Get a key-value difference pair.
+
+    Parameters:
+        key: key to be checked,
+        data1: data from the first file,
+        data2: data from the second file
+        (both represented as dicts).
+
+    Returns:
+        difference pair:
+        {
+            type: nested/changed/unchanged/added/removed,
+            value: {
+                [old_value:] some_value,
+                [new_value: if any,]
+            }
+        }
+    """
+    difference_pair = {}
+    if key not in data1 and key in data2:
+        difference_pair[key] = {
+            'type': 'added',
+            'value': data2[key],
+        }
+    elif key in data1 and key not in data2:
+        difference_pair[key] = {
+            'type': 'removed',
+            'value': data1[key],
+        }
+    elif data1[key] == data2[key]:
+        difference_pair[key] = {
+            'type': 'unchanged',
+            'value': data1[key],
+        }
+    elif isinstance(data1[key], Dict) and isinstance(data2[key], Dict):
+        difference_pair[key] = {
+            'type': 'nested',
+            'value': get_compared_data(data1[key], data2[key]),
+        }
+    else:  # data1[key] != data2[key]
+        difference_pair[key] = {
+            'type': 'changed',
+            'value': {
+                'old value': data1[key],
+                'new value': data2[key],
+            },
+        }
+    return difference_pair
 
 
 def get_compared_data(data1, data2):
@@ -26,38 +78,12 @@ def get_compared_data(data1, data2):
     Returns:
         ordered dict of compared data.
     """
-    data_difference = OrderedDict()
+    data_difference = {}
     keys1, keys2 = set(data1.keys()), set(data2.keys())
-    key_union = sorted(keys1 | keys2)  # now it's a list!!
+    key_union = sorted(keys1 | keys2)
     for key in key_union:
-        if key not in data1 and key in data2:
-            data_difference[key] = {
-                'type': 'added',
-                'value': data2[key],
-            }
-        elif key in data1 and key not in data2:
-            data_difference[key] = {
-                'type': 'removed',
-                'value': data1[key],
-            }
-        elif isinstance(data1[key], Dict) and isinstance(data2[key], Dict):
-            data_difference[key] = {
-                'type': 'nested',
-                'value': get_compared_data(data1[key], data2[key]),
-            }
-        elif data1[key] == data2[key]:
-            data_difference[key] = {
-                'type': 'unchanged',
-                'value': data1[key],
-            }
-        else:
-            data_difference[key] = {
-                'type': 'changed',
-                'value': {
-                    'old value': data1[key],
-                    'new value': data2[key],
-                },
-            }
+        difference_pair = get_difference_pair(key, data1, data2)
+        data_difference.update(difference_pair)
     return data_difference
 
 
